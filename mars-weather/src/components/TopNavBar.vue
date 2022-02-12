1c0d9370acc3db014776c1d49ce77acc77a2b0a7
LeonXZhou <leon.zhou@queensu.ca>
2022-02-12 15:42:33 -0800

    changing project idea

---
+++ mars-weather/src/components/TopNavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <div>asdf</div>
+    <div>asdf</div>
+    <div>asdf</div>
+    <div>asdf</div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TopNavBar",
+};
+</script>
+
+<style scoped>
+nav {
+  display:flex;
+}
+</style>
+

9c63f35ec7fc4bce43aecf98f058296f1a5bf93e
LeonXZhou <leon.zhou@queensu.ca>
2022-02-14 15:30:50 -0800

    added nav structure

---
+++ tarot/src/components/NavItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>This is NavItem</div>
+</template>
+
+<script>
+import NavItem from './NavIem.vue'
+export default {
+  name: "TopNav",
+  components: {NavItem}
+}
+</script>
---
+++ tarot/src/components/TopNav.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav></nav>
+</template>
+
+<script>
+import NavItem from './NavIem.vue'
+export default {
+  name: "TopNav",
+  components: {NavItem}
+}
+</script>

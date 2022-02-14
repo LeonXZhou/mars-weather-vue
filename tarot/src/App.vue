e7057cb00e545e88bc53f88aa31766d2b8f2b65a
LeonXZhou <leon.zhou@queensu.ca>
2022-02-14 15:30:39 -0800

    added structure for CardSet and Card components

---
+++ tarot/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <Card v-bind="cardInfo" />
+</template>
+
+<script>
+import axios from "axios";
+import Card from "./components/Card.vue";
+export default {
+  name: "App",
+  components: { CardSet },
+
+  data() {
+    return {
+      cardInfo: {
+        name: "leon",
+        desc: "adsfasdf",
+      },
+    };
+  },
+
+  created() {
+    axios
+      .get("https://rws-cards-api.herokuapp.com/api/v1/cards/random",{ params: { n: 10 } })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log('asdf');
+        this.cardInfo.name = "no internet :(";
+        this.cardInfo.desc = "no internet :(";
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ tarot/src/components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+    <div>{{ name }}</div>
+    <div>{{ desc }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    name: String,
+    desc: String,
+  },
+};
+
+</script>
+
+<style scoped>
+.card {
+  border-color: aqua;
+  border-style: solid;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+
+  border-radius: 10px;
+
+  width: 10em;
+  height: 13em;
+}
+.card:hover {
+  border-color: blueviolet;
+}
+
+.card > div {
+  border-color: blueviolet;
+  border-style: solid;
+}
+</style>
